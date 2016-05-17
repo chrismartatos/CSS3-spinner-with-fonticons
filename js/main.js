8a00c2f471dfcf31dc4c9886755c149a5c310cd6
Chris Martatos <info@chrismartatos.com>
2016-05-17 16:06:12 -0700

    Clean code

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
 /**  
 	There is many ways to fadeOut the preloader/spinner with Javascript when the window is fully loaded.
-	I prefer to use simple css and classes with a nice transition effect. (light & fast)
-	However here is a very simple way to do it really quick:
+	I prefer to apply classes and use CSS transitions. (light & fast)
+	However here is a very simple way to fade out the preloader with jQuery:
 **/	
 
-/*$(function()
+$(function()
 {
-	//when window is fully load
+	/*Dom ready - trigger function*/
+	//hide_preloader_on_load();
+});
+	
+	
+//Hide preloader when window is fully loaded
+function hide_preloader_on_load()
+{
 	$(window).load(function()
 	{
-		$(".site-preloader").fadeOut('slow',function()
-		{
-			//Do something after the prelader is fadeout for example animate #page or add a class, or whatever.
-		});
+		fade_out_preloader();
 	});
-});*/
-	
+}
+
+//Fade out
+function fade_out_preloader()
+{
+	$(".site-preloader").fadeOut('slow',function()
+	{
+		//Do something after the prelader is fadeout for example animate #page or add a class, or whatever.
+	});
+}
